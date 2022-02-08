01c796ef6a50688cb2988568eb696402d817a721
Alejandro Daza Truque <alejodt995@gmail.com>
2022-02-07 19:05:20 -0500

    feat(S3.14): Use Observer and Subcriber
    How to create and Observer and use the Subscription to retrieve the values from the Observable.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 //const  obs$ = Observable.create()
+
+
+const observer: Observer<any> = {
+    next: value => console.log('siguiente [next]:', value),
+    error: error => console.warn('error [obs]:', error), 
+    complete: () => console.info('Completado [obs]')
+
+}
 
 const obs$ = new Observable<string>( subs => {
     subs.next('Hola');
@@ -9,10 +17,22 @@ const obs$ = new Observable<string>( subs => {
     subs.next('Hola');
     subs.next('Mundo');
 
+    //Forzar un error
+    //const a = undefined;
+    //a.nombre = 'Fernando';
+
+
     subs.complete();
 });
 
-obs$.subscribe(console.log);
+obs$.subscribe(observer);
+
+// 3 argumentos next, error, complete
+// obs$.subscribe(
+//     valor => console.log('next', valor),
+//     error => console.warn('error', error), 
+//     () => console.log('Completado')
+// );
 
 
 
