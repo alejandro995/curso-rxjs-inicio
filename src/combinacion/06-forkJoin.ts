e36313daff3cc4e6b15289abdbf83b53db768d84
Alejandro Daza Truque <alejodt995@gmail.com>
2022-02-10 00:47:38 -0500

    feat(forkJoin): Create exercise to test the forkJoin
    It is useful to use destructing to map every last emitted value from the observables joined.

---
+++ src/combinacion/06-forkJoin.ts
@@ -0,0 +1,28 @@
+import { delay, forkJoin, interval, of, take } from "rxjs";
+
+
+const numeros$ = of(1,2,3,4);
+const interval$ = interval(1000).pipe(
+    take(3)
+);
+const letras$ = of('a', 'b', 'c').pipe(delay(3500));
+
+
+// forkJoin([numeros$, interval$, letras$]).subscribe(console.log)
+
+// forkJoin({numeros$, interval$, letras$}).subscribe(
+//     resp => {
+//    console.log(resp)
+//     }
+    
+// )
+
+ forkJoin({
+     num: numeros$, 
+     int: interval$, 
+     let: letras$}).subscribe(
+    resp => {
+    console.log(resp)
+    }
+    
+ )
